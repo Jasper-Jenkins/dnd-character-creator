c9b58dd333bf31fbf3c27fdcadec2a1ef6728a31
Jasper <ownyourfate@yahoo.com>
2020-10-09 16:43:32 -0600

    Updates

---
+++ precache-manifest.a404ae74351c449d3f13f33bbe7da0d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a331dfb12f619d333af96b95976a8337",
+    "url": "/dnd-character-creator/src/index.html"
+  },
+  {
+    "revision": "5d21c2833e57d32c948a",
+    "url": "/dnd-character-creator/src/static/css/main.5bfa980d.chunk.css"
+  },
+  {
+    "revision": "0e223dfd8035079ac7f8",
+    "url": "/dnd-character-creator/src/static/js/2.fcebd1c9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/dnd-character-creator/src/static/js/2.fcebd1c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d21c2833e57d32c948a",
+    "url": "/dnd-character-creator/src/static/js/main.42d02e57.chunk.js"
+  },
+  {
+    "revision": "0bf0bf9890f993260085",
+    "url": "/dnd-character-creator/src/static/js/runtime-main.3fa11540.js"
+  }
+]);
