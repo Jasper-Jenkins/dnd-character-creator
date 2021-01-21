1453b27f25cd2ab010010a1b9baa00b5917c2131
Jasper <ownyourfate@yahoo.com>
2021-01-21 11:34:12 -0700

    Updates

--- precache-manifest.c42ca233b2632d62af443f0b2dd5e850.js
+++ precache-manifest.c42ca233b2632d62af443f0b2dd5e850.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b3d866043f78129b0ad8ef6f0a76a1ce",
+    "revision": "1472844c5c46d7c2e1d61096a9e4db50",
     "url": "/dnd-character-creator/index.html"
   },
   {
-    "revision": "182b59a4481780c14170",
-    "url": "/dnd-character-creator/static/css/main.8512fa3d.chunk.css"
+    "revision": "a6ed03d465021af4b54e",
+    "url": "/dnd-character-creator/static/css/main.35a58535.chunk.css"
   },
   {
     "revision": "43c207b8b351dec21868",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/dnd-character-creator/static/js/2.fc4bf88f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "182b59a4481780c14170",
-    "url": "/dnd-character-creator/static/js/main.14d3251d.chunk.js"
+    "revision": "a6ed03d465021af4b54e",
+    "url": "/dnd-character-creator/static/js/main.43cc877f.chunk.js"
   },
   {
     "revision": "a5f755cae4520b229306",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dnd-character-creator/precache-manifest.8050acd7e9f6a49d8b63ead4c1a8bd2f.js"
+  "/dnd-character-creator/precache-manifest.c42ca233b2632d62af443f0b2dd5e850.js"
 );
 
 self.addEventListener('message', (event) => {
